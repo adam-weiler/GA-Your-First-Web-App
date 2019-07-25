6173f65e76d1c447dd2305875440c33acd96c25e
adam-weiler <adam.weiler@yahoo.ca>
2019-07-25 13:28:34 -0400

    Adding first url pattern to home_page.

--- my_first_web_app/urls.py
+++ my_first_web_app/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
+def home_page(request):
+    return HttpResponse('Hi, this is the home page!')
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('home/', home_page)
 ]
